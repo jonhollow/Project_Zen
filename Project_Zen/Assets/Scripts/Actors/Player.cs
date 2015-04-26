43c53316787a78c7b7a0a4ea44e4c298711cb297
Jon Hollow <th3h0ll0w0n3@gmail.com>
2015-04-26 15:34:15 -0700

    Player Changes
    Submitting player changes that take into account the changes in the
    input manager.

--- Project_Zen/Assets/Scripts/Actors/Player.cs
+++ Project_Zen/Assets/Scripts/Actors/Player.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent (typeof(Controller2D))]
 public class Player : MonoBehaviour
 {
     #region Fields
 
-    string m_ActionJump = "space";
-    string m_ActionMoveLeft = "left";
-    string m_ActionMoveRight = "right";
+    public int m_PlayerID = 1;
+
+    protected string m_ActionJump;
+    protected string m_ActionHorizontal;
+    protected string m_ActionVertical;
+    protected string m_ActionAttack;
 
     // Jumping variables
     public float m_JumpHeight = 4.0f;
@@ -23,12 +28,17 @@ public class Player : MonoBehaviour
 
     float m_Gravity; // move from player into the map, so each map can screw with gravity?
     float m_JumpVelocity;
-    Vector3 m_Velocity = Vector3.zero;
+    protected Vector3 m_Velocity = Vector3.zero;
     float m_VelocityXSmoothing = 0.0f;
     float m_VelocityYSmoothing = 0.0f;
 
     float m_WallMoveWaitTimer = 0.0f;
     int m_NumAirJumps = 0;
+
+    // Player specific helpers
+    protected float m_AttackChargeTimer = 0.0f;
+    bool m_MovementEnabled = true;
+    bool m_AttacksEnabled = true;
 
     /// <summary>
     /// Handle to the controller
@@ -48,6 +58,12 @@ public class Player : MonoBehaviour
 
         m_Gravity = -(2 * m_JumpHeight) / Mathf.Pow(m_TimeToJumpApex, 2.0f);
         m_JumpVelocity = Mathf.Abs(m_Gravity) * m_TimeToJumpApex;
+
+        // Calculate input strings
+        m_ActionJump = "Player" + m_PlayerID + " Jump";
+        m_ActionHorizontal = "Player" + m_PlayerID + " Horizontal";
+        m_ActionVertical = "Player" + m_PlayerID + " Vertical";
+        m_ActionAttack = "Player" + m_PlayerID + " Attack";
 	}
 
     /// <summary>
@@ -55,14 +71,26 @@ public class Player : MonoBehaviour
     /// </summary>
     void Update()
     {
+        if (m_MovementEnabled) Movement();
+        if (m_AttacksEnabled) Attacks();
+
+        ApplyForces();
+
+        if (m_Velocity.magnitude > 0.0f)
+            m_ControllerHandle.Move(m_Velocity * Time.deltaTime);
+    }
+
+    void Movement()
+    {
         if (m_ControllerHandle.Info.above || m_ControllerHandle.Info.below)
         {
             m_Velocity.y = 0.0f;
         }
 
+        float axisRaw = Input.GetAxis(m_ActionHorizontal);
         float moveX = 0.0f;
-        if (Input.GetKey(m_ActionMoveLeft)) moveX = -1.0f;
-        if (Input.GetKey(m_ActionMoveRight)) moveX = 1.0f;
+        if (axisRaw < -0.3f) moveX = -1.0f;
+        if (axisRaw > 0.3f) moveX = 1.0f;
 
         // if we're on the left wall and we're trying to move right or
         // if we're on the right wall and we're trying to move left
@@ -82,9 +110,9 @@ public class Player : MonoBehaviour
         if (m_ControllerHandle.Info.below)
             m_NumAirJumps = 0;
 
-        if (Input.GetKeyDown(m_ActionJump))
+        if (Input.GetButtonDown(m_ActionJump))
         {
-            if( m_ControllerHandle.Info.below )
+            if (m_ControllerHandle.Info.below)
                 m_Velocity.y = m_JumpVelocity;
             else if (m_ControllerHandle.Info.right)
             {
@@ -96,7 +124,7 @@ public class Player : MonoBehaviour
                 Vector3 angledVelocity = new Vector3(0.5f, 1.0f, 0.0f);
                 m_Velocity = angledVelocity.normalized * m_JumpVelocity;
             }
-            else  if( !m_ControllerHandle.Info.below && m_NumAirJumps < m_InAirJumps )
+            else if (!m_ControllerHandle.Info.below && m_NumAirJumps < m_InAirJumps)
             {
                 m_Velocity.y = m_JumpVelocity;
                 m_NumAirJumps++;
@@ -105,6 +133,10 @@ public class Player : MonoBehaviour
 
         float targetVelocityX = moveX * m_MoveSpeed;
         m_Velocity.x = Mathf.SmoothDamp(m_Velocity.x, targetVelocityX, ref m_VelocityXSmoothing, (m_ControllerHandle.Info.below ? m_AccelerationTimeGrounded : m_AccelerationTimeAirborne));
+    }
+
+    void ApplyForces()
+    {
         m_Velocity.y += m_Gravity * Time.deltaTime;
 
         // Wall sliding
@@ -112,8 +144,11 @@ public class Player : MonoBehaviour
         {
             m_Velocity.y = Mathf.SmoothDamp(m_Velocity.y, m_WallFallSpeed, ref m_VelocityYSmoothing, m_AccelerationTimeWall);
         }
+    }
 
-        m_ControllerHandle.Move(m_Velocity * Time.deltaTime);
+
+    void Attacks()
+    {
     }
 
     #endregion
